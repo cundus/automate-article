81cc33ffd07490958e860eb33622c1cd198b23f5
Sundus <pcinta0@gmail.com>
2024-08-13 15:26:31 +0700

    first commit

---
+++ app/index.ts
@@ -0,0 +1,10 @@
+import app from "./server";
+import { config } from "dotenv";
+
+config();
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+   console.log(`Listening on port ${port}`);
+});
